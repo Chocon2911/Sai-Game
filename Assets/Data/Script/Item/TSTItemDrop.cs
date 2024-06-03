1ce07c305b6cc38757db86e45855983b33a0d286
Chocon2911 <131384762+Chocon2911@users.noreply.github.com>
2024-06-03 05:04:57 -0700

    ep61
    + Update TSTItemDrop, now, it can counts object and calculate rate

--- Assets/Data/Script/Item/TSTItemDrop.cs
+++ Assets/Data/Script/Item/TSTItemDrop.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class TSTItemDrop : JunkAbstract
 {
+    [SerializeField] protected int dropTimes = 0;
+    [SerializeField] protected int dropCount = 0;
+    [SerializeField] protected int rate;
+
     protected virtual void Start()
     {
         InvokeRepeating(nameof(this.DropItem), 2, 1f);
@@ -14,6 +18,12 @@ public class TSTItemDrop : JunkAbstract
         List<ItemDropRate> itemDropRates = new List<ItemDropRate>();
         itemDropRates = this.junkManager.ShootableObjSO.DropList;
 
-        ItemDropSpawner.Instance.DropItem(itemDropRates, transform.parent.position, Quaternion.Euler(0, 0, 0));
+        Vector2 spawnPos = transform.parent.position;
+        Quaternion spawnRot = Quaternion.Euler(0, 0, 0);
+        List<ItemDropSO> droppedItems = ItemDropSpawner.Instance.DropItem(itemDropRates, spawnPos, spawnRot);
+
+        this.dropTimes += itemDropRates.Count;
+        this.dropCount += droppedItems.Count;
+        this.rate = this.dropCount * 100 / this.dropTimes;
     }
 }
